0f80f3123fa19dcf91349032045f14b28b656b28
cs-ai-hesham-elithy <hesham.elithy.int@clear-sky.ai>
2026-05-11 12:43:28 +0300

    Initial MCP server

---
+++ src/mcp/server.py
@@ -0,0 +1,7 @@
+from mcp.fast.server import FastMCP
+
+server = FastMCP("mcp-server")
+
+# --------------------Context Graph Tools/Resources--------------------
+
+# --------------------Process Orchestration Tools/Resources--------------------
